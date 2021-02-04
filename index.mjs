144a7947c0225738008db1af926b4e2c1888c2d8
Audra <73372336+IamAudra@users.noreply.github.com>
2021-02-04 15:02:47 -0500

    format index.mjs file to import data from data.mjs

---
+++ index.mjs
@@ -0,0 +1,20 @@
+import { data }  from "./data.mjs";
+
+const { name } = data
+
+const { location } = data
+
+const {education} = data
+
+const {hobbies} = data
+
+const {baking} = data
+
+const { movies} = data
+
+console.log(name)
+console.log(education)
+console.log(location);
+console.log(hobbies);
+console.log(baking);
+console.log(movies);
