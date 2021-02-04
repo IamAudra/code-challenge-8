ad54506867db5725689761cb5d49b3e946e2b478
Audra <73372336+IamAudra@users.noreply.github.com>
2021-02-04 15:01:12 -0500

    Created data file for app

---
+++ data.mjs
@@ -0,0 +1,57 @@
+export const data = {
+
+    name: {
+        fName: "Audra",
+        lName: "J"
+    },
+
+    location: {
+        city: "Boston",
+        state: "MA"
+    },
+
+    education: {
+            first: "Fitchburg State University, MBA in progress",
+            second: "Harvard University, B.A.",
+        },
+
+        certificates: {
+            PhoenixFamilies: {
+                first: "HIPAA",
+                second: "Emergency Management",
+                third: "Trauma-Informed Care",
+            },
+
+                     ulemMsimboProgram: {
+                 first:"MSIMBO Coding Bootcamp",
+            
+        },
+    },
+    
+    hobbies: {
+        reading: {
+            book1: "Holy Bible",
+                author: "",
+            book2: "Persuasion",
+                author: "Austen, Jane",
+            book3: "A Raisin in the Sun",
+                author: "Hansberry, Lorraine",
+
+        baking: {
+                book1: "Cakes",
+                book2: "Cookies",
+                book3: "Savory Foods"
+            },
+
+        movies: {
+                movie1:"Aliens",
+                movie2: "Pride and Prejudice (1992)",
+                movie3: "The Great Doctor",
+                movie4: "A Woman Called Moses",
+
+        }    
+    },
+},
+}
+
+
